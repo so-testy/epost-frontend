9d23aee7ef1b5d834051071fa38caf2580c37608
Ladanov Nikolay <ladanov_n@mail.ru>
2020-06-20 15:18:07 +0300

    design fot catalog page

--- src/pages/catalog/index.jsx
+++ src/pages/catalog/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-// import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarker } from '@fortawesome/free-solid-svg-icons'
 import Dropdown from 'react-dropdown'
 
 import { withRouter } from 'react-router-dom';
@@ -21,6 +22,7 @@ const cities = [
 ];
 
 const Catalog = (props) => {
+    const [isLoading, setIsLoading] = useState(false);
     const [departmentNum, setDepartmentNum] = useState(35);
     const [departmentName, setDepartmentName] = useState('hospital');
     const [city, setCity] = useState('Москва');
@@ -30,46 +32,61 @@ const Catalog = (props) => {
     const find = async (event) => {
         event.preventDefault();
         
-        setOrganizations([{
-            name: 'Городская Клиническая Больница № 15 им. Ф.И. Иноземцева',
-            ourEmail: '35-больница-москва.епочта.рф',
-            originEmail: 'admin@inozemtcev.ru',
-        }])
+        setIsLoading(true);
+        setTimeout(() => {
+            setIsLoading(false);
+            setOrganizations([{
+                name: 'Городская Клиническая Больница № 15 им. Ф.И. Иноземцева',
+                address: "ул. Фортунатовкая, д.1, Москва",
+                ourEmail: '35-больница-москва.епочта.рф',
+                originEmail: 'admin@inozemtcev.ru',
+            }])
+        }, 1000);
     };
 
     return (
         <div className="catalog-page">
             <div className="form-wrapper">
-                <form>
-                    <fieldset>
-                        <label>Номер</label>
-                        <input 
-                            className="department-num" 
-                            type="text" 
-                            value={departmentNum} 
-                            onChange={(e) => setDepartmentNum(e.target.value)} 
-                        />
-                    </fieldset>
-                    <fieldset>
-                        <label>Тип организации</label>
-                        <Dropdown
-                            className="department-name" 
-                            value={departmentName}
-                            onChange={(department) => setDepartmentName(department.value)}
-                            options={departmentNames}
-                        />
-                    </fieldset>
-                    <fieldset>
-                        <label>Город</label>
-                        <Dropdown
-                            className="city" 
-                            value={city}
-                            onChange={(city) => setCity(city.value)}
-                            options={cities}
-                        />
-                    </fieldset>
-                    <button onClick={find}>Найти</button>
-                </form>
+                <div className="main">
+                    <div className="title">
+                        <span className="logo">
+                            <span>Е</span>
+                            <span>ПОЧТА</span>
+                            <span>.РФ</span>
+                        </span>
+                        поиск государственных организаций
+                    </div>
+                    <form>
+                        <fieldset>
+                            <label>Номер</label>
+                            <input 
+                                className="department-num" 
+                                type="text" 
+                                value={departmentNum} 
+                                onChange={(e) => setDepartmentNum(e.target.value)} 
+                            />
+                        </fieldset>
+                        <fieldset>
+                            <label>Тип организации</label>
+                            <Dropdown
+                                className="department-name" 
+                                value={departmentName}
+                                onChange={(department) => setDepartmentName(department.value)}
+                                options={departmentNames}
+                            />
+                        </fieldset>
+                        <fieldset>
+                            <label>Город</label>
+                            <Dropdown
+                                className="city" 
+                                value={city}
+                                onChange={(city) => setCity(city.value)}
+                                options={cities}
+                            />
+                        </fieldset>
+                        <button disabled={isLoading} onClick={find}>Найти</button>
+                    </form>
+                </div>
             </div>
             <div className="emails">
                 {
@@ -78,12 +95,22 @@ const Catalog = (props) => {
                             <div className="name">
                                 { organization.name }
                             </div>
+                            <div className="address">
+                                <span>
+                                    <FontAwesomeIcon icon={faMapMarker} />
+                                </span>
+                                { organization.address }
+                            </div>
                             <div className="emails">
-                                <div className="our">
-                                    { organization.ourEmail }
+                                <div className="title">
+                                    Для связи
                                 </div>
                                 <div className="origin">
                                     { organization.originEmail }
+                                </div>
+                                <br />
+                                <div className="our">
+                                    { organization.ourEmail }
                                 </div>
                             </div>
                         </div>
